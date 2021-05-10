d5c6c361b1e04af81cb2a0798714fa8a88345bfd
dlamb22 <domlambwebdev@gmail.com>
2021-05-10 17:23:06 -0500

    remove cloudinary vars

--- controllers/articles.js
+++ controllers/articles.js
@@ -90,12 +90,6 @@ exports.newArticle = async (req, res) => {
       .trim()
       .replace(/\.[^/.]+$/, '')}`;
 
-    cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.CLOUD_API_KEY,
-      api_secret: process.env.CLOUD_API_SECRET,
-    });
-
     const uploadResult = await cloudinary.uploader.upload(file.tempFilePath, {
       secure: true,
       folder: 'uploads',
