2b29e28eaa008d21065ed1ceb464d609daf965e2
dlamb22 <domlambwebdev@gmail.com>
2021-05-10 16:48:25 -0500

    update

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_DB, {
+    await mongoose.connect(process.env.MONGO_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
