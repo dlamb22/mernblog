5a6add5bfbdfd70bdc5c6b9db8b08bc7889d8781
dlamb22 <domlambwebdev@gmail.com>
2021-05-10 16:23:24 -0500

    update

--- controllers/articles.js
+++ controllers/articles.js
@@ -90,12 +90,6 @@ exports.newArticle = async (req, res) => {
       .toString(36)
       .substr(2, 11)}${file.name.trim().replace(/\.[^/.]+$/, '')}`;
 
-    cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.CLOUD_API_KEY,
-      api_secret: process.env.CLOUD_API_SECRET,
-    });
-
     const uploadResult = await cloudinary.uploader.upload(file.tempFilePath, {
       secure: true,
       folder: 'uploads',
